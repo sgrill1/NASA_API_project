00cd7deeb1fbac5d52f071e59b4d9674b5bfc02f
Sam Grill <sgrill@spartaglobal.com>
2018-11-08 16:47:57 +0000

    build a get earth date method

--- src/main/java/RoverPhotosParser.java
+++ src/main/java/RoverPhotosParser.java
@@ -16,7 +16,13 @@ public class RoverPhotosParser {
     }
     public JSONArray getResultsJSONObject(){return (JSONArray) imagesJSON.get("photos");}
     public String getImageSource(){
-        JSONObject singleImage = (JSONObject) imagesResults.get(0);
+        JSONObject singleImage = (JSONObject) imagesResults.get(20);
         return singleImage.get("img_src").toString ();
+    }
+
+    public String getEarthDate(){
+        JSONObject singleEarthDate = (JSONObject) imagesResults.get(1);
+        return singleEarthDate.get("earth_date").toString();
+
     }
 }
